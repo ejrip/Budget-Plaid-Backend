43ad0eb53db37d8cb014453897c15d910ddc5c6c
ejrip <elirippey@gmail.com>
2026-06-12 16:38:00 -0400

    Serve static files from the 'public' directory

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.options('*', cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 const config = new Configuration({
 basePath: PlaidEnvironments.production,
